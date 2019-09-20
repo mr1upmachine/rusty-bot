6e97fda30be4658c62f333bb300433b041f167d2
Ty Simpson <ty@tysimpson.com>
2019-09-20 18:32:13 -0400

    Karma only given in channels flagged 'meme'

--- src/utilities/statistics.ts
+++ src/utilities/statistics.ts
@@ -1,8 +1,8 @@
 import { FieldValue, Firestore } from '@google-cloud/firestore';
 import { Client, Message, MessageReaction, User } from 'discord.js';
 
-// TODO: Only give karma in meme channels
 // TODO: Update member post count on deletion? Would require channel and message partials, probably not worth the extra effort
+// TODO: Optimize
 
 exports.addReaction = async (client: Client, msgReact: MessageReaction, user: User, firestore: Firestore) => {
   const guild = msgReact.message.guild;
@@ -15,13 +15,28 @@ exports.addReaction = async (client: Client, msgReact: MessageReaction, user: Us
     reactionCount: FieldValue.increment(1),
   }, {merge: true});
 
-  if (user !== msgReact.message.author) {
-    // Add to user karma
-    const userRef = guildRef.collection('members').doc(msg.member!.id);
-    const addToMember = userRef.set({
-      karma: FieldValue.increment(1),
-    }, {merge: true});
-  }
+  let meme = false;
+
+  const getChannel = guildRef.collection('channels').doc(msg.channel.id).get()
+  .then((doc) => {
+    if (!doc.exists) {
+      msg.channel.send('Error retrieving channel!');
+      return;
+    } else {
+      if (doc.data()!.meme) { meme = doc.data()!.meme; }
+
+      if (user !== msgReact.message.author) {
+        // Add to user karma
+        const userRef = guildRef.collection('members').doc(msg.member!.id);
+        const addToMember = userRef.set({
+          karma: FieldValue.increment(1),
+        }, {merge: true});
+      }
+    }
+  })
+  .catch((err) => {
+    msg.channel.send('Error retrieving channel info: ' + err);
+  });
 };
 
 exports.removeReaction = async (client: Client, msgReact: MessageReaction, user: User, firestore: Firestore) => {
@@ -35,13 +50,27 @@ exports.removeReaction = async (client: Client, msgReact: MessageReaction, user:
     reactionCount: FieldValue.increment(-1),
   }, {merge: true});
 
-  if (user !== msgReact.message.author) {
-    // Remove from user karma
-    const userRef = guildRef.collection('members').doc(msg.member!.id);
-    const addToMember = userRef.set({
-      karma: FieldValue.increment(-1),
-    }, {merge: true});
-  }
+  let meme = false;
+  const getChannel = guildRef.collection('channels').doc(msg.channel.id).get()
+  .then((doc) => {
+    if (!doc.exists) {
+      msg.channel.send('Error retrieving channel!');
+      return;
+    } else {
+      if (doc.data()!.meme) { meme = doc.data()!.meme; }
+
+      if (user !== msgReact.message.author && meme) {
+        // Remove from user karma
+        const userRef = guildRef.collection('members').doc(msg.member!.id);
+        const addToMember = userRef.set({
+          karma: FieldValue.increment(-1),
+        }, {merge: true});
+      }
+    }
+  })
+  .catch((err) => {
+    msg.channel.send('Error retrieving channel info: ' + err);
+  });
 };
 
 exports.messageSent = async (client: Client, msg: Message, firestore: Firestore) => {
