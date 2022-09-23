ce4df7355f888e43a75c30dbf1e51ba396e47aca
Sean Hale <sean@mr1upmachine.com>
2022-09-23 15:22:12 -0400

    rename about command to version

--- src/commands/version/index.ts
+++ src/commands/version/index.ts
@@ -3,15 +3,13 @@ import type { ChatInputCommandInteraction } from 'discord.js';
 import { Command } from '../../types/command.js';
 import { getVersion } from '../../utilities/get-version.js';
 
-class AboutCommand extends Command {
+class VersionCommand extends Command {
   public readonly description = 'Displays version information';
 
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     const version = await getVersion();
-    await interaction.reply(
-      `Hi, I'm Rusty, mascot of the Rusty's Bois server! My current version is \`${version}\`!`
-    );
+    await interaction.reply(`My current version is \`${version}\`!`);
   }
 }
 
-export default AboutCommand;
+export default VersionCommand;
